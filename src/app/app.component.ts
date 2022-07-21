96d6a27d47fadde1c7f2bf41fe0b53ed567a2668
dharmendramishra-19 <71832492+dharmendramishra-19@users.noreply.github.com>
2022-07-21 16:08:13 +0530

    null comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-app';
+  title:string = 'angular-app';
+  color: string ="red";
+  value:number=0;
+  load(area: string) {
+    console.log(area);
+    this.value= parseInt(area)+5;
+  }
+  birthday= new Date(2021, 3, 14);
+  named="";
+ clearValue() {
+   this.named="";
+ }
+ userTasks:any[]=[];
+
+ addTask(task:string){
+  this.userTasks.push({id:this.userTasks.length,name:task});
+ }
+ deleteTask(task:number){
+  console.log(task);
+  this.userTasks=this.userTasks.filter(taskno=>taskno.id!==task)
+ }
+
+ data:number=10;
+ random(){
+  this.data=Math.floor((Math.random()*6)+1);
+ }
+ 
 }
